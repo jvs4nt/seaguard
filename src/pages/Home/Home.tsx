dfc69815e346de63fd1d69ba859aca13b7cfd4a4
João <joao@MacBook-Pro-de-MacBook.local>
2024-06-04 22:39:38 -0300

    add video teste

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -56,8 +56,14 @@ const Home : React.FC = () => {
                 </TransparentBigCard>
 
                 <BigCard>
-                    
-                </BigCard>
+                <iframe 
+                        width="100%" 
+                        height="600" 
+                        src="https://www.youtube.com/embed/byOxbddwnJI" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </BigCard> 
             </HomeContainer>
         </>
     )
